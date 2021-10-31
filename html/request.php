1db544449eb428516a58a03ebdca3550d9b4c1ad
Anthony <atossell91@gmail.com>
2021-10-31 17:13:07 -0600

    error

--- html/request.php
+++ html/request.php
@@ -14,19 +14,20 @@
 			$conn= new mysqli($servername, $username, $password, $dbname);
 			
 			if ($conn->connect_error) {
-				die("Database Connection failed: " . $conn->connect_error);
+				echo "An error ocurred";
 			}
+			else {
+				$sql = "SELECT * FROM GIRLS WHERE BODYTYPE = \"BBW\";";
+				$result = $conn->query($sql);
 
-			$sql = "SELECT * FROM GIRLS WHERE BODYTYPE = \"BBW\";";
-			$result = $conn->query($sql);
-
-			if ($result->num_rows > 0) {
-				while($row = $result->fetch_assoc()) {
-					echo "Name: " . $row["NAME"] . " - Body type: " . $row["BODYTYPE"] . "<br>";
+				if ($result->num_rows > 0) {
+					while($row = $result->fetch_assoc()) {
+						echo "Name: " . $row["NAME"] . " - Body type: " . $row["BODYTYPE"] . "<br>";
+					}
+				} else {
+					echo "No results.";
 				}
-			} else {
-				echo "No results.";
-			}
+			}	
 			$conn->close();*/
 		?>
 	</body>
