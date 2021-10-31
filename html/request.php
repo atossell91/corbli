ad1bd1d503a139f3064933808eda80a937eb5ceb
Anthony <atossell91@gmail.com>
2021-10-31 17:16:07 -0600

    maybe fixed now?

--- html/request.php
+++ html/request.php
@@ -14,20 +14,18 @@
 			$conn= new mysqli($servername, $username, $password, $dbname);
 			
 			if ($conn->connect_error) {
-				echo "An error ocurred";
+				die ("An error ocurred: " . $conn->connect_error);
 			}
-			else {
-				$sql = "SELECT * FROM GIRLS WHERE BODYTYPE = \"BBW\";";
-				$result = $conn->query($sql);
+			$sql = "SELECT * FROM GIRLS WHERE BODYTYPE = \"BBW\";";
+			$result = $conn->query($sql);
 
-				if ($result->num_rows > 0) {
-					while($row = $result->fetch_assoc()) {
-						echo "Name: " . $row["NAME"] . " - Body type: " . $row["BODYTYPE"] . "<br>";
-					}
-				} else {
-					echo "No results.";
+			if ($result->num_rows > 0) {
+				while($row = $result->fetch_assoc()) {
+					echo "Name: " . $row["NAME"] . " - Body type: " . $row["BODYTYPE"] . "<br>";
 				}
-			}	
+			} else {
+				echo "No results.";
+			}
 			$conn->close();
 		?>
 	</body>
