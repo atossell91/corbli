aaf5fe8e8423fc867319f7cc45f773abb59d8f11
Anthony <atossell91@gmail.com>
2021-11-01 14:56:34 -0600

    h

--- php/secret.php
+++ php/secret.php
@@ -2,7 +2,7 @@
 <?php
 	session_start();
 	if (isset($_SESSION['corbli_isLoggedIn']) && $_SESSION['corbli_isLoggedIn'] ==true) {
-		header('Location: ../html/secret.html')
+		header('Location: ../html/secret.html');
 	}
 	else {
 		header('Location: ../index.php');
