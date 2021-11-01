f614b040b69a9932cb8de3ffd8f1bebfd1995fa4
Anthony <atossell91@gmail.com>
2021-11-01 16:08:38 -0600

    light

--- php/secret.php
+++ php/secret.php
@@ -1,10 +1,25 @@
-<!DOCTYPE html>
 <?php
 	session_start();
-	if (isset($_SESSION['corbli_isLoggedIn']) && $_SESSION['corbli_isLoggedIn'] ==true) {
-		header('Location: ../html/secret.html');
-	}
-	else {
+	if (!(isset($_SESSION['corbli_isLoggedIn']) && $_SESSION['corbli_isLoggedIn'] ==true)) {
 		header('Location: ../index.php');
+		exit;
 	}
 ?>
+<!DOCTYPE html>
+<html>
+	<head>
+		<link href="../css/styles.css" rel="stylesheet" type="text/css">
+		<script type="text/javascript" src="../scripts/calc_hours.js"></script>
+	</head>
+	<body>
+		<div class="title">
+			Corbli
+		</div>
+		<div class="links">
+			<a href="../index.html">Home</a>
+			<a href="about.html">About</a>
+			<a href="hours_calc.html">Calculate Hours</a>
+		</div>
+		<img src="../images/bbw.jpg">
+	</body>
+</html>
--- scripts/scriptjs.js
+++
@@ -1,3 +0,0 @@
-function sayHello () {
-  window.alert('Hello')
-}
