6d5a336a7058c20b82681a57146832138a68906d
Anthony <atossell91@gmail.com>
2021-11-01 14:49:17 -0600

    r

--- php/secret.php
+++ php/secret.php
@@ -2,11 +2,9 @@
 <?php
 	session_start();
 	if (isset($_SESSION['corbli_isLoggedIn']) && $_SESSION['corbli_isLoggedIn'] ==true) {
-		echo "In";
-		//echo get_file_contents('Location: www.corbli.com/html/secret.html');
+		echo get_file_contents('Location: www.corbli.com/html/secret.html');
 	}
 	else {
-		echo "Out";
-		header('Location: index.html');
+		header('Location: ../index.html');
 	}
 ?>
