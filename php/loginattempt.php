0bf280bf67539c018867be755d788034781fd48b
Anthony <atossell91@gmail.com>
2021-11-04 13:13:59 -0600

    t

--- php/loginattempt.php
+++ php/loginattempt.php
@@ -22,7 +22,7 @@
     }
 
     $row=$res->fetch_assoc();
-    if ($row['username']==$_POST['uname'] && password_verify($_POST['pword'],$row['password'])) {
+    if ($row['username']==$_POST['username'] && password_verify($_POST['password'],$row['password'])) {
         $_SESSION['corbli_isLoggedIn']=true;
         $_SESSION['user']=$_POST['uname'];
         header('Location: ../index.php');
