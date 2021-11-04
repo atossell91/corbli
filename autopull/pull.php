af52505f9d147742cce2a1e256560ae4134b53df
Anthony <atossell91@gmail.com>
2021-11-04 12:55:29 -0600

    &

--- autopull/pull.php
+++ autopull/pull.php
@@ -3,7 +3,7 @@
 // Use in the “Post-Receive URLs” section of your GitHub repo.
 
 //if ( $_POST['payload'] ) {
-$output = shell_exec('cd /var/www/corbli && git pull 2>1');
+$output = shell_exec('cd /var/www/corbli && git pull 2>&1');
 echo 'Whiskey' . '<br>';
 echo '|' . $output . '| <br>';
 //}
